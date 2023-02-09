7bb8f2b9800b66a904b87aee20fb07861de1b27e
Hanatan <hanatan079@gmail.com>
2023-02-09 20:23:02 +0900

    リザルド画面

---
+++ DietGame/Assets/Scripts/TotalScore.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+
+public class TotalScore : MonoBehaviour
+{
+    public Text scoreText;
+    int score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = GameManager.GetScore();
+        scoreText.text = string.Format("{0} カロリー", score);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

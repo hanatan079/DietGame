275d84d09f2b182bcaa4a167e53de6e5d0717373
Hanatan <hanatan079@gmail.com>
2023-02-10 12:00:35 +0900

    生成場所修正

--- DietGame/Assets/Scripts/FoodGenerate.cs
+++ DietGame/Assets/Scripts/FoodGenerate.cs
@@ -34,7 +34,7 @@ public class FoodGenerate : MonoBehaviour
         for(int i=0; i < count; i++)
         {
             //生成する場所指定
-            Vector3 posY = new Vector3(Random.Range(-2.5f,2.5f),10f,-10f);
+            Vector3 posY = new Vector3(Random.Range(-1.4f,-0.4f),10f,-10f);
             food = Instantiate(foodPrefab, posY, Quaternion.identity);
 
             //親オブジェクトを変更する

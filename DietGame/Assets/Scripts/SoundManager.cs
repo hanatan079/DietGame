d04922e2328790f63cf3355becee2a959ca0ab6d
Hanatan <hanatan079@gmail.com>
2023-02-11 09:11:20 +0900

    PlayBGM関数作成

--- DietGame/Assets/Scripts/SoundManager.cs
+++ DietGame/Assets/Scripts/SoundManager.cs
@@ -8,22 +8,29 @@ public class SoundManager : MonoBehaviour
     public static SoundManager single;
 
     //BGM格納
-    public AudioSource[] bgm;
+    public AudioSource bgm;
 
     //AudioClip格納
-    public AudioClip audioClip;
+    public AudioClip[] bgmClip;
 
 
-    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        PlayBGM(0);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    //再生(1.スタートリザルド　2.プレイ中)
+    public void PlayBGM(int x)
+    {
+        bgm.clip = bgmClip[x];
+        bgm.Play();
     }
 }
